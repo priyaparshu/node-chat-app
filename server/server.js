bf5aa5dc4b2abd1514b5d35007c34b22c78c11b9
priyaparshu <priyaparshu@gmail.com>
2019-03-10 16:06:56 -0500

    greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -10,21 +10,38 @@ const io = socketIO(server);
 pathdir = path.join(__dirname, '../public')
 
 app.use(express.static(pathdir));
+
+
 io.on('connection', (socket) => {
-
     console.log('new user connected');
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'welcome to chat app',
+        createdAt: new Date().getTime()
+    });
 
-
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'A new user has joined',
+        createdAt: new Date().getTime()
+    });
 
 
     socket.on('createMessage', (e) => {
         console.log(e)
+        // to send the msg to everyone including myself
         io.emit('newMessage', {
             from: e.from,
             text: e.text,
             createdAt: new Date().getTime()
         })
-    })
+        // to send the msg to everyone excluding myself
+        // socket.broadcast.emit('newMessage', {
+        //     from: e.from,
+        //     text: e.text,
+        //     createdAt: new Date().getTime()
+        // });
+    });
 
 
 
